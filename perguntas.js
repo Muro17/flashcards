7f370c84485bc9703ff7f706bf25aecca284ecb3
Muro <105938976+Muro17@users.noreply.github.com>
2024-09-26 08:21:43 -0300

    f

--- perguntas.js
+++ perguntas.js
@@ -2,121 +2,121 @@ criaCartao(
     'Química',
     'Quais são os componentes do gás natural?',
     'Metano e Etano')
-    
-    criaCartao(
-        'Matemática',
-        'Quanto é a metade de 2+2',
-        '3')
 
-     criaCartao(
-        'Biologia',
-        'Aonde são quebradas as moléculas de carboídratos no ato da digestão?',
-        'Na boca')
+criaCartao(
+    'Matemática',
+    'Quanto é a metade de 2+2',
+    '3')
 
-        criaCartao(
-            'Literatura',
-            'No livro Noite na Taverna, qual dos personagens teve uma história confirmada como verdadeira',
-            'Johann'
-        )
+criaCartao(
+    'Biologia',
+    'Aonde são quebradas as moléculas de carboídratos no ato da digestão?',
+    'Na boca')
 
-        criaCartao(
-            'Física',
-            'Qual primeira lei de Ohmn',
-            'U = R x i'
-        )
+criaCartao(
+    'Literatura',
+    'No livro Noite na Taverna, qual dos personagens teve uma história confirmada como verdadeira',
+    'Johann'
+)
 
-        criaCartao(
-            'Energia e Astronomia',
-            'Qual processo é realizado no núcleo do Sol',
-            'Fusão Nuclear'
-        )
+criaCartao(
+    'Física',
+    'Qual primeira lei de Ohmn',
+    'U = R x i'
+)
 
-        criaCartao(
-            'Saúde e bem-estar',
-            'Qual a importância de ler um livro?',
-            'Estimulo de criatividade e aumento de vocabulário'
-        )
+criaCartao(
+    'Energia e Astronomia',
+    'Qual processo é realizado no núcleo do Sol',
+    'Fusão Nuclear'
+)
 
-        criaCartao(
-            'Robótica',
-            'O que são jumpers?',
-            'Os fios'
-        )
+criaCartao(
+    'Saúde e bem-estar',
+    'Qual a importância de ler um livro?',
+    'Estimulo de criatividade e aumento de vocabulário'
+)
 
-        criaCartao(
-            'História',
-            'Em que ano foi o golpe militar no Brasil',
-            '1964'
-        )
+criaCartao(
+    'Robótica',
+    'O que são jumpers?',
+    'Os fios'
+)
 
-        criaCartao(
-            'Geografia',
-            'Quais são os 6 continentes?',
-            'Ásia, Américas, África, Europa, Oceania, Antártida'
-        )
+criaCartao(
+    'História',
+    'Em que ano foi o golpe militar no Brasil',
+    '1964'
+)
 
-        criaCartao(
-            'Filosofia',
-            'Quais são os 3 filósofos contratualistas',
-            'Locke, Rousseau, Hobbs'
-        )
+criaCartao(
+    'Geografia',
+    'Quais são os 6 continentes?',
+    'Ásia, Américas, África, Europa, Oceania, Antártida'
+)
 
-        criaCartao(
-            'Sociologia',
-            'Quais são os 3 sociólogos clássicos',
-            'Marx, Durkheim, Weber'
-        )
-        
-        criaCartao(
-            'Educação Financeira',
-            'Qual a fórmula dos juros simples',
-            'J = C . i . T'
-        )
+criaCartao(
+    'Filosofia',
+    'Quais são os 3 filósofos contratualistas',
+    'Locke, Rousseau, Hobbs'
+)
 
-        criaCartao(
-            'História',
-            'Aonde começou a revolução Industrial',
-            'Inglaterra'
-        )
+criaCartao(
+    'Sociologia',
+    'Quais são os 3 sociólogos clássicos',
+    'Marx, Durkheim, Weber'
+)
 
-        criaCartao(
-            'História',
-            'Qual o nome da aliança que ganhou a primeira guerra mundial',
-            'Tríplice Entente'
-        )
+criaCartao(
+    'Educação Financeira',
+    'Qual a fórmula dos juros simples',
+    'J = C . i . T'
+)
 
-        criaCartao(
-            'Geografia',
-            'O que são ilhas de calor?',
-            'São domos localizados no centro da cidade que possuem temperaturas mais elevadas'
-        )
+criaCartao(
+    'História',
+    'Aonde começou a revolução Industrial',
+    'Inglaterra'
+)
 
-        criaCartao(
-            'Física',
-            'O que não muda em uma onda?',
-            'Frequência'
-        )
+criaCartao(
+    'História',
+    'Qual o nome da aliança que ganhou a primeira guerra mundial',
+    'Tríplice Entente'
+)
 
-        criaCartao(
-            'Filosofia',
-            'O que é um mundo sensível para Platão?',
-            'O mundo dos toques e percepções'
-        )
+criaCartao(
+    'Geografia',
+    'O que são ilhas de calor?',
+    'São domos localizados no centro da cidade que possuem temperaturas mais elevadas'
+)
 
-        criaCartao(
-            'Saúde e Bem-Estar',
-            'Qual o nome dos orgão usado para respiração',
-            'Pulmão'
-        )
+criaCartao(
+    'Física',
+    'O que não muda em uma onda?',
+    'Frequência'
+)
 
-        criaCartao(
-            'Biologia',
-            'O que a mitocôndria faz?',
-            'Respiração'
-        )
+criaCartao(
+    'Filosofia',
+    'O que é um mundo sensível para Platão?',
+    'O mundo dos toques e percepções'
+)
 
-        criaCartao(
-            'Química',
-            'Qual a fórmula do sal de cozinha?',
-            'NaCl'
-        )
+criaCartao(
+    'Saúde e Bem-Estar',
+    'Qual o nome dos orgão usado para respiração',
+    'Pulmão'
+)
+
+criaCartao(
+    'Biologia',
+    'O que a mitocôndria faz?',
+    'Respiração'
+)
+
+criaCartao(
+    'Química',
+    'Qual a fórmula do sal de cozinha?',
+    'NaCl'
+)
