13b1686aacd6524642655ab9c2fabd9da0bf99af
Muro <105938976+Muro17@users.noreply.github.com>
2024-09-26 08:20:33 -0300

    dsadas

--- perguntas.js
+++ perguntas.js
@@ -1,123 +1,122 @@
 criaCartao(
-    'Química'
-    'Quais são os componentes do gás natural?'
+    'Química',
+    'Quais são os componentes do gás natural?',
     'Metano e Etano')
     
     criaCartao(
-        'Matemática'
-        'Quanto é a metade de 2+2'
+        'Matemática',
+        'Quanto é a metade de 2+2',
         '3')
 
      criaCartao(
-        'Biologia'
-        'Aonde são quebradas as moléculas de carboídratos no ato da digestão?'
-        'Na boca'
-        )
+        'Biologia',
+        'Aonde são quebradas as moléculas de carboídratos no ato da digestão?',
+        'Na boca')
 
         criaCartao(
-            'Literatura'
-            'No livro Noite na Taverna, qual dos personagens teve uma história confirmada como verdadeira'
+            'Literatura',
+            'No livro Noite na Taverna, qual dos personagens teve uma história confirmada como verdadeira',
             'Johann'
         )
 
         criaCartao(
-            'Física'
-            'Qual primeira lei de Ohmn'
+            'Física',
+            'Qual primeira lei de Ohmn',
             'U = R x i'
         )
 
         criaCartao(
-            'Energia e Astronomia'
-            'Qual processo é realizado no núcleo do Sol'
+            'Energia e Astronomia',
+            'Qual processo é realizado no núcleo do Sol',
             'Fusão Nuclear'
         )
 
         criaCartao(
-            'Saúde e bem-estar'
-            'Qual a importância de ler um livro?'
+            'Saúde e bem-estar',
+            'Qual a importância de ler um livro?',
             'Estimulo de criatividade e aumento de vocabulário'
         )
 
         criaCartao(
-            'Robótica'
-            'O que são jumpers?'
+            'Robótica',
+            'O que são jumpers?',
             'Os fios'
         )
 
         criaCartao(
-            'História'
-            'Em que ano foi o golpe militar no Brasil'
+            'História',
+            'Em que ano foi o golpe militar no Brasil',
             '1964'
         )
 
         criaCartao(
-            'Geografia'
-            'Quais são os 6 continentes?'
+            'Geografia',
+            'Quais são os 6 continentes?',
             'Ásia, Américas, África, Europa, Oceania, Antártida'
         )
 
         criaCartao(
-            'Filosofia'
-            'Quais são os 3 filósofos contratualistas'
+            'Filosofia',
+            'Quais são os 3 filósofos contratualistas',
             'Locke, Rousseau, Hobbs'
         )
 
         criaCartao(
-            'Sociologia'
-            'Quais são os 3 sociólogos clássicos'
+            'Sociologia',
+            'Quais são os 3 sociólogos clássicos',
             'Marx, Durkheim, Weber'
         )
         
         criaCartao(
-            'Educação Financeira'
-            'Qual a fórmula dos juros simples'
+            'Educação Financeira',
+            'Qual a fórmula dos juros simples',
             'J = C . i . T'
         )
 
         criaCartao(
-            'História'
-            'Aonde começou a revolução Industrial'
+            'História',
+            'Aonde começou a revolução Industrial',
             'Inglaterra'
         )
 
         criaCartao(
-            'História'
-            'Qual o nome da aliança que ganhou a primeira guerra mundial'
+            'História',
+            'Qual o nome da aliança que ganhou a primeira guerra mundial',
             'Tríplice Entente'
         )
 
         criaCartao(
-            'Geografia'
-            'O que são ilhas de calor?'
+            'Geografia',
+            'O que são ilhas de calor?',
             'São domos localizados no centro da cidade que possuem temperaturas mais elevadas'
         )
 
         criaCartao(
-            'Física'
-            'O que não muda em uma onda?'
+            'Física',
+            'O que não muda em uma onda?',
             'Frequência'
         )
 
         criaCartao(
-            'Filosofia'
-            'O que é um mundo sensível para Platão?'
+            'Filosofia',
+            'O que é um mundo sensível para Platão?',
             'O mundo dos toques e percepções'
         )
 
         criaCartao(
-            'Saúde e Bem-Estar'
-            'Qual o nome dos orgão usado para respiração'
+            'Saúde e Bem-Estar',
+            'Qual o nome dos orgão usado para respiração',
             'Pulmão'
         )
 
         criaCartao(
-            'Biologia'
-            'O que a mitocôndria faz?'
+            'Biologia',
+            'O que a mitocôndria faz?',
             'Respiração'
         )
 
         criaCartao(
-            'Química'
-            'Qual a fórmula do sal de cozinha?'
+            'Química',
+            'Qual a fórmula do sal de cozinha?',
             'NaCl'
         )
